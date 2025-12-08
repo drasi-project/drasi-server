ec673a6d7cfabbb510c4776deab7b86aa82bce13
Agent of Reality <agentx@agentofreality.com>
2025-12-08 13:47:45 -0800

    Source and Reaction factories use plugin's builder
    Signed-off-by: Agent of Reality <agentx@agentofreality.com>

--- src/factories.rs
+++ src/factories.rs
@@ -55,25 +55,76 @@ use crate::config::{ReactionConfig, SourceConfig};
 /// ```
 pub async fn create_source(config: SourceConfig) -> Result<Box<dyn Source + 'static>> {
     let source: Box<dyn Source + 'static> = match &config {
-        SourceConfig::Mock { id, config: c, .. } => {
-            use drasi_source_mock::MockSource;
-            Box::new(MockSource::new(id, c.clone())?)
+        SourceConfig::Mock {
+            id,
+            auto_start,
+            config: c,
+            ..
+        } => {
+            use drasi_source_mock::MockSourceBuilder;
+            Box::new(
+                MockSourceBuilder::new(id)
+                    .with_data_type(&c.data_type)
+                    .with_interval_ms(c.interval_ms)
+                    .with_auto_start(*auto_start)
+                    .build()?,
+            )
         }
-        SourceConfig::Http { id, config: c, .. } => {
-            use drasi_source_http::HttpSource;
-            Box::new(HttpSource::new(id, c.clone())?)
+        SourceConfig::Http {
+            id,
+            auto_start,
+            config: c,
+            ..
+        } => {
+            use drasi_source_http::HttpSourceBuilder;
+            Box::new(
+                HttpSourceBuilder::new(id)
+                    .with_config(c.clone())
+                    .with_auto_start(*auto_start)
+                    .build()?,
+            )
         }
-        SourceConfig::Grpc { id, config: c, .. } => {
-            use drasi_source_grpc::GrpcSource;
-            Box::new(GrpcSource::new(id, c.clone())?)
+        SourceConfig::Grpc {
+            id,
+            auto_start,
+            config: c,
+            ..
+        } => {
+            use drasi_source_grpc::GrpcSourceBuilder;
+            Box::new(
+                GrpcSourceBuilder::new(id)
+                    .with_config(c.clone())
+                    .with_auto_start(*auto_start)
+                    .build()?,
+            )
         }
-        SourceConfig::Postgres { id, config: c, .. } => {
-            use drasi_source_postgres::PostgresReplicationSource;
-            Box::new(PostgresReplicationSource::new(id, c.clone())?)
+        SourceConfig::Postgres {
+            id,
+            auto_start,
+            config: c,
+            ..
+        } => {
+            use drasi_source_postgres::PostgresSourceBuilder;
+            Box::new(
+                PostgresSourceBuilder::new(id)
+                    .with_config(c.clone())
+                    .with_auto_start(*auto_start)
+                    .build()?,
+            )
         }
-        SourceConfig::Platform { id, config: c, .. } => {
-            use drasi_source_platform::PlatformSource;
-            Box::new(PlatformSource::new(id, c.clone())?)
+        SourceConfig::Platform {
+            id,
+            auto_start,
+            config: c,
+            ..
+        } => {
+            use drasi_source_platform::PlatformSourceBuilder;
+            Box::new(
+                PlatformSourceBuilder::new(id)
+                    .with_config(c.clone())
+                    .with_auto_start(*auto_start)
+                    .build()?,
+            )
         }
     };
 
@@ -163,74 +214,128 @@ pub fn create_reaction(config: ReactionConfig) -> Result<Box<dyn Reaction + 'sta
         ReactionConfig::Log {
             id,
             queries,
+            auto_start,
             config,
-            ..
         } => {
-            use drasi_reaction_log::LogReaction;
-            Ok(Box::new(LogReaction::new(&id, queries, config)))
+            use drasi_reaction_log::LogReactionBuilder;
+            let mut builder = LogReactionBuilder::new(&id)
+                .with_queries(queries)
+                .with_auto_start(auto_start);
+            if let Some(template) = config.added_template {
+                builder = builder.with_added_template(template);
+            }
+            if let Some(template) = config.updated_template {
+                builder = builder.with_updated_template(template);
+            }
+            if let Some(template) = config.deleted_template {
+                builder = builder.with_deleted_template(template);
+            }
+            Ok(Box::new(builder.build()))
         }
         ReactionConfig::Http {
             id,
             queries,
+            auto_start,
             config,
-            ..
         } => {
-            use drasi_reaction_http::HttpReaction;
-            Ok(Box::new(HttpReaction::new(&id, queries, config)))
+            use drasi_reaction_http::HttpReactionBuilder;
+            Ok(Box::new(
+                HttpReactionBuilder::new(&id)
+                    .with_queries(queries)
+                    .with_auto_start(auto_start)
+                    .with_config(config)
+                    .build()?,
+            ))
         }
         ReactionConfig::HttpAdaptive {
             id,
             queries,
+            auto_start,
             config,
-            ..
         } => {
-            use drasi_reaction_http_adaptive::AdaptiveHttpReaction;
-            Ok(Box::new(AdaptiveHttpReaction::new(&id, queries, config)))
+            use drasi_reaction_http_adaptive::HttpAdaptiveReactionBuilder;
+            Ok(Box::new(
+                HttpAdaptiveReactionBuilder::new(&id)
+                    .with_queries(queries)
+                    .with_auto_start(auto_start)
+                    .with_config(config)
+                    .build()?,
+            ))
         }
         ReactionConfig::Grpc {
             id,
             queries,
+            auto_start,
             config,
-            ..
         } => {
-            use drasi_reaction_grpc::GrpcReaction;
-            Ok(Box::new(GrpcReaction::new(&id, queries, config)))
+            use drasi_reaction_grpc::GrpcReactionBuilder;
+            Ok(Box::new(
+                GrpcReactionBuilder::new(&id)
+                    .with_queries(queries)
+                    .with_auto_start(auto_start)
+                    .with_config(config)
+                    .build()?,
+            ))
         }
         ReactionConfig::GrpcAdaptive {
             id,
             queries,
+            auto_start,
             config,
-            ..
         } => {
-            use drasi_reaction_grpc_adaptive::AdaptiveGrpcReaction;
-            Ok(Box::new(AdaptiveGrpcReaction::new(&id, queries, config)))
+            use drasi_reaction_grpc_adaptive::GrpcAdaptiveReactionBuilder;
+            Ok(Box::new(
+                GrpcAdaptiveReactionBuilder::new(&id)
+                    .with_queries(queries)
+                    .with_auto_start(auto_start)
+                    .with_config(config)
+                    .build()?,
+            ))
         }
         ReactionConfig::Sse {
             id,
             queries,
+            auto_start,
             config,
-            ..
         } => {
-            use drasi_reaction_sse::SseReaction;
-            Ok(Box::new(SseReaction::new(&id, queries, config)))
+            use drasi_reaction_sse::SseReactionBuilder;
+            Ok(Box::new(
+                SseReactionBuilder::new(&id)
+                    .with_queries(queries)
+                    .with_auto_start(auto_start)
+                    .with_config(config)
+                    .build()?,
+            ))
         }
         ReactionConfig::Platform {
             id,
             queries,
+            auto_start,
             config,
-            ..
         } => {
-            use drasi_reaction_platform::PlatformReaction;
-            Ok(Box::new(PlatformReaction::new(&id, queries, config)?))
+            use drasi_reaction_platform::PlatformReactionBuilder;
+            Ok(Box::new(
+                PlatformReactionBuilder::new(&id)
+                    .with_queries(queries)
+                    .with_auto_start(auto_start)
+                    .with_config(config)
+                    .build()?,
+            ))
         }
         ReactionConfig::Profiler {
             id,
             queries,
+            auto_start,
             config,
-            ..
         } => {
-            use drasi_reaction_profiler::ProfilerReaction;
-            Ok(Box::new(ProfilerReaction::new(&id, queries, config)))
+            use drasi_reaction_profiler::ProfilerReactionBuilder;
+            Ok(Box::new(
+                ProfilerReactionBuilder::new(&id)
+                    .with_queries(queries)
+                    .with_auto_start(auto_start)
+                    .with_config(config)
+                    .build()?,
+            ))
         }
     }
 }
