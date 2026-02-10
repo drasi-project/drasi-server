50464904cf899a2ddd74e222996b5e90c23da1e4
Agent of Reality <agentx@agentofreality.com>
2026-02-10 14:30:47 -0800

    Update devcontainer: add DB_HOST env var and use cargo install

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -19,12 +19,9 @@ sudo apt-get update && sudo apt-get install -y \
     clang \
     libclang-dev
 
-# Build Drasi Server in release mode
+# Build and install Drasi Server
 echo "🔨 Building Drasi Server (this may take a few minutes)..."
-cargo build --release
-
-# Create symlink for consistent binary access
-ln -sf ./target/release/drasi-server ./drasi-server
+cargo install --path . --root . --locked
 
 # Make scripts executable
 if [ -d "examples/getting-started/scripts" ]; then
